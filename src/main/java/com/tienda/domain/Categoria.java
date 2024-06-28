1a260901ccd1d138ea50b565aea047526501bec3
Diego Lopez Garro <114270024+dalg97@users.noreply.github.com>
2024-06-27 22:39:05 -0600

    Agregando el start-class en pom y editando una variable

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -14,7 +14,7 @@ public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L; 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column)name="id_categoria")
+    @Column(name="id_categoria")
     
     private Long idCategoria;
     private String descripcion;
